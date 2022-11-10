6b63ce663463fa2e649298014ea1b45ccecc224c
Hendi Irwanto <hi.hendiirwanto@gmail.com>
2022-11-10 17:10:27 +0700

    page for admin

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard.index');
+        $users = User::all();
+        $roles = Role::all();
+        return view('dashboard.index', compact('users', 'roles'));
     }
 }
